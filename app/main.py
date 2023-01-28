e6cbc6eec9fd63a69a70d69aac961ae90ebae367
FernandoCelmer <email@fernandocelmer.com>
2023-01-28 17:15:50 -0300

    ⚙️  NO-TASK - Update Settings

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from mangum import Mangum
 
 from app import __version__
 from app.api.auth import auth
 from app.api.v1.routers import v1
+from app.core.auth import Auth
 
 
 app = FastAPI(
@@ -17,17 +19,24 @@ app = FastAPI(
 app.include_router(auth, prefix="/auth", tags=["Auth"])
 app.include_router(v1, prefix="/v1")
 
+security = HTTPBearer()
+auth_handler = Auth()
+
 
 @app.get("/status", include_in_schema=False)
-def get_status():
+def get_status(credentials: HTTPAuthorizationCredentials = Security(security)):
     """Get status of messaging server."""
-    return ({"status":  "it's alive"})
+    token = credentials.credentials
+    if(auth_handler.decode_token(token)):
+        return ({"status":  "it's alive"})
 
 
 @app.get("/error", include_in_schema=False)
-def get_status():
+def get_status(credentials: HTTPAuthorizationCredentials = Security(security)):
     """Get error of messaging server."""
-    raise
+    token = credentials.credentials
+    if(auth_handler.decode_token(token)):
+        raise
 
 
 handler = Mangum(app)
