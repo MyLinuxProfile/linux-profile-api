7508d02c66326acfa592d3197af9043fe55aa64b
FernandoCelmer <email@fernandocelmer.com>
2023-01-28 17:00:57 -0300

    📌 ISSUE-#4 - Update settings

--- app/main.py
+++ app/main.py
@@ -9,16 +9,25 @@ from app.api.v1.routers import v1
 app = FastAPI(
     title="LinuxProfile API",
     description="Linux Profile Project API",
-    version=__version__)
+    version=__version__,
+    docs_url=None,
+    redoc_url=None)
+
 
 app.include_router(auth, prefix="/auth", tags=["Auth"])
 app.include_router(v1, prefix="/v1")
 
 
 @app.get("/status", include_in_schema=False)
-async def get_status():
+def get_status():
     """Get status of messaging server."""
     return ({"status":  "it's alive"})
+
+
+@app.get("/error", include_in_schema=False)
+def get_status():
+    """Get error of messaging server."""
+    raise
 
 
 handler = Mangum(app)
