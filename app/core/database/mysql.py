df56de204a1caf98397a8a12b352ec625ebf7c3b
FernandoCelmer <email@fernandocelmer.com>
2023-01-29 22:33:56 -0300

    📌 ISSUE-#16 - Integration with Sentry.io

--- app/core/database/mysql.py
+++ app/core/database/mysql.py
@@ -8,7 +8,7 @@ config = set_up()
 engine = create_engine(
     url=config.get("MYSQL_DATABASE_URL"),
     connect_args={},
-    pool_recycle=300,
+    pool_recycle=420,
     pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
